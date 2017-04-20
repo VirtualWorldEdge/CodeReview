21e06e418ccdfd17b1d6c4084c48a2beac1805ad
virtualTeamGit <364938622@qq.com>
2017-04-20 15:39:13 -0400

    first commit
    123

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = null;
+
+            if(1==1 || p.ToString() == "123")
+            {
+                Console.WriteLine("top");
+            }
+            else
+            {
+                Console.WriteLine("bot");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
